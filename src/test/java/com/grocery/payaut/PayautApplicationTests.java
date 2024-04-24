3ca68b0c8a88ffcf764e628713c441ae598ec690
devkiloton <dev.kiloton@gmail.com>
2024-04-24 16:26:10 +0200

    Initial commit

---
+++ src/test/java/com/grocery/payaut/PayautApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grocery.payaut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayautApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
