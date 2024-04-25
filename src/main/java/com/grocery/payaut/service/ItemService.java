fb66bd5e613b10842739099409dce122353ff6e4
devkiloton <dev.kiloton@gmail.com>
2024-04-25 15:43:46 +0200

    Adding methods to ItemService

--- src/main/java/com/grocery/payaut/service/ItemService.java
+++ src/main/java/com/grocery/payaut/service/ItemService.java
@@ -7,7 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.grocery.payaut.dto.ItemDTO;
+import com.grocery.payaut.dto.ItemDiscountDTO;
+import com.grocery.payaut.dto.ItemDiscountSlabDTO;
+import com.grocery.payaut.model.Discount;
+import com.grocery.payaut.model.DiscountSlab;
 import com.grocery.payaut.model.Item;
+import com.grocery.payaut.repository.IDiscountRepository;
+import com.grocery.payaut.repository.IDiscountSlabRepository;
 import com.grocery.payaut.repository.IItemRepository;
 
 @Service
@@ -16,21 +23,62 @@ public class ItemService {
     @Autowired
     private IItemRepository itemRepository;
 
-    public ResponseEntity<com.grocery.payaut.model.Item> upsertItem(Item item) {
-        Optional<com.grocery.payaut.model.Item> itemOptional = itemRepository.findById(item.getItemId());
-        if (itemOptional.isPresent()) {
+    @Autowired
+    private IDiscountRepository discountRepository;
+
+    @Autowired
+    private IDiscountSlabRepository discountSlabRepository;
+
+    public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public ResponseEntity<Item> upsertItem(ItemDTO itemDTO) {
+        if (itemDTO.getItemId() != null) {
+            Optional<Item> itemOptional = itemRepository.findById(itemDTO.getItemId());
             Item existingItem = itemOptional.get();
-            // existingItem.setCreatedAt(item.getCreatedAt());
-            // existingItem.setType(item.getType());
+            existingItem.setType(itemDTO.getType());
+            existingItem.setUnit(itemDTO.getUnit());
+            existingItem.setPrice(itemDTO.getPrice());
+            existingItem.setDiscount(itemDTO.getDiscount());
             Item savedItem = itemRepository.save(existingItem);
             return ResponseEntity.ok(savedItem);
         }
 
-        Item savedItem = itemRepository.save(item);
+        Item newItem = new Item();
+        newItem.setType(itemDTO.getType());
+        newItem.setUnit(itemDTO.getUnit());
+        newItem.setPrice(itemDTO.getPrice());
+        newItem.setDiscount(itemDTO.getDiscount());
+        System.out.println("newItem: " + itemDTO.getDiscount());
+        Item savedItem = itemRepository.save(newItem);
         return ResponseEntity.ok(savedItem);
     }
 
-    public List<Item> getAllItems() {
-        return itemRepository.findAll();
+    public ResponseEntity<Discount> upsertItemDiscount(ItemDiscountDTO itemdDiscountDTO) {
+        if (itemdDiscountDTO.getDiscountId() != null) {
+            Optional<Discount> discountOptional = discountRepository.findById(itemdDiscountDTO.getDiscountId());
+            Discount existingDiscount = discountOptional.get();
+            existingDiscount.setDiscountSlabs(itemdDiscountDTO.getDiscountSlabs());
+            existingDiscount.setDiscountUnit(itemdDiscountDTO.getDiscountUnit());
+            Discount savedDiscount = discountRepository.save(existingDiscount);
+            return ResponseEntity.ok(savedDiscount);
+        }
+        Discount newDiscount = new Discount();
+        newDiscount.setDiscountSlabs(itemdDiscountDTO.getDiscountSlabs());
+        newDiscount.setDiscountUnit(itemdDiscountDTO.getDiscountUnit());
+        Discount savedDiscount = discountRepository.save(newDiscount);
+        return ResponseEntity.ok(savedDiscount);
+    }
+
+    public ResponseEntity<DiscountSlab> updateDiscountSlab(ItemDiscountSlabDTO itemDiscountSlabDTO) {
+        Optional<DiscountSlab> discountSlabOptional = discountSlabRepository
+                .findById(itemDiscountSlabDTO.getDiscountSlabId());
+        DiscountSlab existingDiscountSlab = discountSlabOptional.get();
+        existingDiscountSlab.setIsConstantSlab(itemDiscountSlabDTO.isConstantSlab());
+        existingDiscountSlab.setUnitsToGetDiscount(itemDiscountSlabDTO.getUnitsToGetDiscount());
+        existingDiscountSlab.setDiscountAmount(itemDiscountSlabDTO.getDiscountAmount());
+        DiscountSlab savedDiscountSlab = discountSlabRepository.save(existingDiscountSlab);
+        return ResponseEntity.ok(savedDiscountSlab);
     }
 }
