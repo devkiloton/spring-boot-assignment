7bf2f462a18137f7e67335aad765e2a58209d46b
devkiloton <dev.kiloton@gmail.com>
2024-04-25 22:13:49 +0200

    Creating CartDTO

---
+++ src/main/java/com/grocery/payaut/dto/CartDTO.java
@@ -0,0 +1,20 @@
+package com.grocery.payaut.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.grocery.payaut.model.CartItem;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CartDTO {
+    private Long cartId;
+    private List<CartItem> cartItems;
+    private LocalDateTime cartDate; // Assuming LocalDateTime for cart date
+}
