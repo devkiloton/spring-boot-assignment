2ea0177d3eb6704d80b1bed2a366e1fdddf60021
devkiloton <dev.kiloton@gmail.com>
2024-04-25 11:08:58 +0200

    Enabling transactions

--- src/main/java/com/grocery/payaut/PayautApplication.java
+++ src/main/java/com/grocery/payaut/PayautApplication.java
@@ -2,8 +2,10 @@ package com.grocery.payaut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class PayautApplication {
 
 	public static void main(String[] args) {
