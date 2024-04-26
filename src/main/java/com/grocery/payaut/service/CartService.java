48e754e414c0d85ffc0f4bb03bea7f82bcdfbc3f
devkiloton <dev.kiloton@gmail.com>
2024-04-26 04:21:18 +0200

    Splitting checkout method

--- src/main/java/com/grocery/payaut/service/CartService.java
+++ src/main/java/com/grocery/payaut/service/CartService.java
@@ -32,71 +32,73 @@ public class CartService {
 
     public ResponseEntity<List<CartItem>> postCheckoutCart(CartDTO cartDTO) {
         final List<CartItem> cartItems = cartDTO.getCartItems().stream().map(cartItem -> {
-            // Get the item from the cartItem
             final Long itemId = cartItem.getCartItemId();
-            // Retrieve the item from the database
             final Item item = itemRepository.findById(itemId).get();
-            // Get the discount from the item
             final Discount discount = item.getDiscount();
-            // Get the discount slab from the discount
             final int quantity = cartItem.getQuantity();
             final double price = item.getPrice();
+            cartItem.setTotalPrice(price * quantity);
+            cartItem.setTotalDiscount(0);
 
             if (discount == null) {
-                System.out.println("1");
-                cartItem.setTotalPrice(price * quantity);
-                cartItem.setTotalDiscount(0);
                 return cartItem;
             }
+
             final DiscountSlab discountSlab = discount.getDiscountSlabs().stream()
                     .filter(slab -> slab.getUnitsToGetDiscount() <= quantity).findFirst().orElse(null);
+
             if (discount.getIsConstantSlab()) {
-                // split that in finals later
-                final double finalDiscount = (int) Math.floor(quantity /
-                        discountSlab.getUnitsToGetDiscount())
-                        * discountSlab.getDiscountAmount();
-                cartItem.setTotalPrice(price * quantity);
-                cartItem.setTotalDiscount(finalDiscount);
-                System.out.println("2");
-                return cartItem;
+                return applyConstantSlab(cartItem, item, price, quantity, discountSlab);
             }
             switch (item.getType()) {
                 case BREADS:
-                    final long breadAge = item.getCreatedAt().until(LocalDateTime.now(), ChronoUnit.DAYS);
-
-                    if (breadAge > 6) {
-                        throw new IllegalArgumentException("Bread is expired");
-                    }
-                    cartItem.setTotalPrice(price * quantity);
-                    cartItem.setTotalDiscount(0);
-                    if (breadAge >= 3) {
-                        cartItem.setQuantity(quantity + 1);
-                        cartItem.setTotalDiscount(price * 1);
-                        cartItem.setTotalPrice(price * (quantity + 1));
-                    }
-                    if (breadAge == 6) {
-                        cartItem.setQuantity(quantity + 2);
-                        cartItem.setTotalDiscount(price * 2);
-                        cartItem.setTotalPrice(price * (quantity + 2));
-                    }
-                    return cartItem;
+                    return applyBreadRule(cartItem, item, price, quantity);
                 case VEGETABLES:
-                    final double finalValue = quantity * price
-                            - (quantity * price * discountSlab.getDiscountAmount() / 100);
-                    cartItem.setTotalPrice(quantity * price);
-                    cartItem.setTotalDiscount((price * quantity) - finalValue);
-                    return cartItem;
+                    return applyItemDiscountRule(cartItem, item, price, quantity, discountSlab);
                 default:
                     return cartItem;
             }
-            // applyItemDiscountRule();
         }).toList();
         return ResponseEntity.ok(cartItems);
     }
 
-    // THESE METHODS SHOULD DEFINITELY NOT BE IN THE CONTROLLER
-    public void applyItemDiscountRule() {
-        // This method is not implemented
+    private CartItem applyBreadRule(CartItem cartItem, Item item, double price, int quantity) {
+        final long breadAge = item.getCreatedAt().until(LocalDateTime.now(), ChronoUnit.DAYS);
+
+        if (breadAge > 6) {
+            throw new IllegalArgumentException("Bread is expired");
+        }
+        if (breadAge >= 3) {
+            cartItem.setQuantity(quantity + 1);
+            cartItem.setTotalDiscount(price * 1);
+            cartItem.setTotalPrice(price * (quantity + 1));
+        }
+        if (breadAge == 6) {
+            cartItem.setQuantity(quantity + 2);
+            cartItem.setTotalDiscount(price * 2);
+            cartItem.setTotalPrice(price * (quantity + 2));
+        }
+        return cartItem;
+    }
+
+    private CartItem applyItemDiscountRule(CartItem cartItem, Item item, double price, int quantity,
+            DiscountSlab discountSlab) {
+        final double finalValue = quantity * price
+                - (quantity * price * discountSlab.getDiscountAmount() / 100);
+        cartItem.setTotalPrice(quantity * price);
+        cartItem.setTotalDiscount((price * quantity) - finalValue);
+        return cartItem;
+    }
+
+    private CartItem applyConstantSlab(CartItem cartItem, Item item, double price, int quantity,
+            DiscountSlab discountSlab) {
+        // split that in finals later
+        final double finalDiscount = (int) Math.floor(quantity /
+                discountSlab.getUnitsToGetDiscount())
+                * discountSlab.getDiscountAmount();
+        cartItem.setTotalPrice(price * quantity);
+        cartItem.setTotalDiscount(finalDiscount);
+        return cartItem;
     }
 
 }
