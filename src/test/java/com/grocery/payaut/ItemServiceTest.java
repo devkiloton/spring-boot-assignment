5d6367d6a0c8eaf2e3db66ff43e938f6b5e28ad3
devkiloton <dev.kiloton@gmail.com>
2024-04-27 11:24:26 +0200

    Creating a couple of tests

---
+++ src/test/java/com/grocery/payaut/ItemServiceTest.java
@@ -0,0 +1,55 @@
+package com.grocery.payaut;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+import com.grocery.payaut.enumerator.ItemTypes;
+import com.grocery.payaut.enumerator.MeasurementUnits;
+import com.grocery.payaut.model.Item;
+import com.grocery.payaut.repository.IItemRepository;
+import com.grocery.payaut.service.ItemService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ItemServiceTest {
+
+    @InjectMocks
+    ItemService itemService;
+
+    @Mock
+    IItemRepository itemRepository;
+
+    Item itemRecord = new Item();
+
+    @BeforeEach
+    public void setUp() {
+        this.itemRecord.setDiscount(null);
+        this.itemRecord.setName("nullS");
+        this.itemRecord.setPrice(0.5);
+        this.itemRecord.setType(ItemTypes.BEERS);
+        this.itemRecord.setUnit(MeasurementUnits.PIECE);
+    }
+
+    @Test
+    public void should_return_all_items_successfully() {
+        when(this.itemRepository.findAll()).thenReturn(Collections.singletonList(this.itemRecord));
+
+        ResponseEntity<List<Item>> items = this.itemService.getAllItems();
+
+        assertEquals(Collections.singletonList(itemRecord), items.getBody());
+        verify(itemRepository).findAll();
+        verifyNoMoreInteractions(itemRepository);
+    }
+
+}
