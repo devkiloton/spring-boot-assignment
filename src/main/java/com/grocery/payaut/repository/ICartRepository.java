a90ec7dad8ceb622663adee461cc207d79e75403
devkiloton <dev.kiloton@gmail.com>
2024-04-28 22:03:04 +0200

    Removing CartRepository

--- src/main/java/com/grocery/payaut/repository/ICartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grocery.payaut.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.grocery.payaut.model.Cart;
-
-public interface ICartRepository extends JpaRepository<Cart, Long> {
-
-}
