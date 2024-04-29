636efe502f9210293d32f09355b51ffd485e5579
devkiloton <dev.kiloton@gmail.com>
2024-04-29 14:26:33 +0200

    Updating data.sql

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -16,6 +16,6 @@ insert into discount_slab (discount_id,discount_amount,units_to_get_discount) va
 insert into item (item_price, created_at, discount_id, item_name, item_type, item_unit) values
 (0.01, '2024-04-24 10:00:00', 1, 'Vegetables', 'VEGETABLES', 'GRAM'),
 (0.5, '2024-04-24 11:00:00', 2, 'Dutch Beer', 'BEERS', 'PIECE'),
-(1.0, '2024-04-19 11:00:00', 3, 'Bread', 'BREADS', 'PIECE'),
+(1.0, '2024-04-26 11:00:00', 3, 'Bread', 'BREADS', 'PIECE'),
 (0.75, '2024-04-24 11:00:00', 4, 'Belgium Beer', 'BEERS', 'PIECE'),
 (1.0, '2024-04-24 11:00:00', 5, 'German Beer', 'BEERS', 'PIECE');
